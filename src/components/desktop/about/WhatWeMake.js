f6489b044d7dea79a58b1f02a2bb30c9b6b73bf2
SarahEvelynn Johnson <sarah@5280startupsolutions.co>
2018-11-07 13:10:47 -0700

    formatting

--- src/components/desktop/about/WhatWeMake.js
+++ src/components/desktop/about/WhatWeMake.js
@@ -3,41 +3,39 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default function WhatWeMake() {
   return (
-    <div>
-      <div>
-        <h1> What We Make </h1>
-        <p>
-          The way we look at it, the marijuana in our ice cream is just an added
-          bonus to an already tremendous frozen treat. Our first three flavors
-          are a fun twist on the traditional staples of ice cream.
-        </p>
-        <p>
-          Vanilla Chilla, Berry High, and Mountain Mint
-          <br />
-          (Have pictures below each flavor of each package)
-        </p>
-        <p>
-          Rest assured each one of these delicious flavors has been tasted and
-          perfected, but we believe ice cream shouldn’t just taste great, it
-          should be made from great ingredients as well. That is why each of
-          these flavors has been created with the best ingredients available
-          from our local suppliers.
-        </p>
-        <p>
-          We offer our signature flavors in 2 sizes, 10 oz as well as ___(insert
-          oz of smaller single serving size)____. Both contain only one legal,
-          single dose of active THC. Our thought was we wanted you to be able to
-          eat as much of our ice cream as your heart desires without
-          overindulging in the marijuana aspect of our product. The pint being
-          for when you want to indulge in the flavor, and the single serving
-          being for when you want to indulge in the experience.
-        </p>
-        <p>
-          We are currently taking suggestions for new flavors! Tell us what you
-          want to taste next and you never know - you just might find it on the
-          shelf at your local dispensary!
-        </p>
-      </div>
+    <div className="aboutSection" id="about2">
+      <h1> What We Make </h1>
+      <p>
+        The way we look at it, the marijuana in our ice cream is just an added
+        bonus to an already tremendous frozen treat. Our first three flavors are
+        a fun twist on the traditional staples of ice cream.
+      </p>
+      <p>
+        Vanilla Chilla, Berry High, and Mountain Mint
+        <br />
+        (Have pictures below each flavor of each package)
+      </p>
+      <p>
+        Rest assured each one of these delicious flavors has been tasted and
+        perfected, but we believe ice cream shouldn’t just taste great, it
+        should be made from great ingredients as well. That is why each of these
+        flavors has been created with the best ingredients available from our
+        local suppliers.
+      </p>
+      <p>
+        We offer our signature flavors in 2 sizes, 10 oz as well as ___(insert
+        oz of smaller single serving size)____. Both contain only one legal,
+        single dose of active THC. Our thought was we wanted you to be able to
+        eat as much of our ice cream as your heart desires without overindulging
+        in the marijuana aspect of our product. The pint being for when you want
+        to indulge in the flavor, and the single serving being for when you want
+        to indulge in the experience.
+      </p>
+      <p>
+        We are currently taking suggestions for new flavors! Tell us what you
+        want to taste next and you never know - you just might find it on the
+        shelf at your local dispensary!
+      </p>
     </div>
   );
 }
---
+++ src/components/desktop/location/Index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+export default function Location() {
+  return (
+    <div>
+    <h1>Our Locations</h1>
+    </div>
+  );
+}
